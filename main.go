08e1b833f2516c3fde97c28d61738fa1a56d7dbe
Jens Zeilund <jzs@users.noreply.github.com>
2016-12-13 20:59:11 +0100

    Added images and contact success page.

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			// read post args,
+			// TODO: Do something with the form value content.
+			fmt.Println(r.FormValue("Name"))
+			fmt.Println(r.FormValue("Email"))
+			fmt.Println(r.FormValue("Phone"))
+			fmt.Println(r.FormValue("Message"))
+		}
+
+		tmpl, err := template.ParseFiles("contact.html")
+		if err != nil {
+			panic(err)
+		}
+		tmpl.Execute(w, nil)
+	})
+
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	fmt.Println("Serving on :8080")
 	http.ListenAndServe(":8080", nil)
