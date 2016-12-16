017ada1a22e8e13df3ef7ccb4e8482dbb9b81ea4
Jens Zeilund <jzs@users.noreply.github.com>
2016-12-16 14:39:35 +0100

    Fixed layout of portfolio, Added slack integration for contact form

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 )
+
+type SlackMsg struct {
+	Text string `json:"text"`
+}
 
 func main() {
 	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			// read post args,
 			// TODO: Do something with the form value content.
-			fmt.Println(r.FormValue("Name"))
-			fmt.Println(r.FormValue("Email"))
-			fmt.Println(r.FormValue("Phone"))
-			fmt.Println(r.FormValue("Message"))
+			name := r.FormValue("Name")
+			email := r.FormValue("Email")
+			phone := r.FormValue("Phone")
+			message := r.FormValue("Message")
+
+			// url for webhook to slack...
+			// https://hooks.slack.com/services/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
+			// curl -X POST --data-urlencode 'payload={"channel": "#websitecontacts", "username": "webhookbot", "text": "This is posted to #websitecontacts and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+
+			// payload={"text": "A very important thing has occurred! <https://alert-system.com/alerts/1234|Click here> for details!"}
+			data := bytes.NewBuffer(nil)
+			enc := json.NewEncoder(data)
+
+			msg := SlackMsg{Text: fmt.Sprintf("Yay! A person wants to be contacted\nName: %v\nEmail: %v\nPhone: %v\nMessage: %v", name, email, phone, message)}
+
+			err := enc.Encode(msg)
+			if err != nil {
+				panic(err)
+			}
+
+			resp, err := http.Post("https://hooks.slack.com/services/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", "application/json", data)
+			if err != nil {
+				panic(err)
+			}
+			if resp.StatusCode != http.StatusOK {
+				panic("wrong status")
+			}
+
 		}
 
 		tmpl, err := template.ParseFiles("contact.html")
