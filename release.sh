777839657f60448b1ffcceefcd9b594378d5808c
Jens Zeilund <jzs@sketchground.dk>
2017-08-12 17:49:49 +0200

    Fix missing contact.html in release build

--- release.sh
+++ release.sh
@@ -4,4 +4,5 @@ cp -r img dist-sketchground/www/img
 cp -r open dist-sketchground/www/open
 cp -r fonts dist-sketchground/www/fonts
 cp -r index.html dist-sketchground/www/index.html
+cp -r contact.html dist-sketchground/www/contact.html
 go build -o dist-sketchground/sketchground *.go
